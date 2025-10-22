655ecd3e985a36e239b98432902bb5d81b619d79
Daniel-Haupt <qxsirdanielgamingxp@gmail.com>
2025-10-22 19:23:34 +0200

    Third Commit: Trying to add image

---
+++ HomePage.tsx
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import React from 'react';
+
+
+const bgImage = require('./.jpeg');
+
+
+export default function App() {
+  return (
+    <ImageBackground source={bgImage} style={styles.background}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
